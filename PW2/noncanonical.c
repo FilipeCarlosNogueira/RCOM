f9ac560fbf4dcbd22976289b258f423ef0fecc36
FilipeCarlosNogueira <43604899+FilipeCarlosNogueira@users.noreply.github.com>
2019-10-08 16:32:55 +0100

    State machine fixed. Message print. Indentation fixed

--- PW2/noncanonical.c
+++ PW2/noncanonical.c
@@ -8,41 +8,119 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
-#define A 0x03
-#define FLAG 0x7e
-#define UA 0x07
-#define CSET 0x03
 
+#define FLAG 0x7e
+#define A 0x03
+#define C 0x03
 
 volatile int STOP=FALSE;
 
+int fd;
+char res;
+char message[5];
+unsigned char UA[5];
+
+int stateMachine(char aux, int state){
+  switch(state){
+      case 0:
+          if(aux==FLAG){
+            message[state] = aux;
+            state++;
+            return state;
+          }
+          else return state;
+      break;
+
+      case 1:
+          if(aux==A){
+            message[state] = aux;
+            state++;
+            return state;
+          }
+          else return state;
+      break;
+
+      case 2:
+          if(aux==C){
+            message[state] = aux;
+            state++;
+            return state;
+          }
+          else if(aux==FLAG){
+              state--;
+              return state;
+          }
+      break;
+
+      case 3:
+          if(aux==UA[3]){
+            message[state] = aux;
+            state++;
+            return state;
+          }
+          else return state;
+      break;
+
+      case 4:
+          if(aux==FLAG){
+            message[state] = aux;
+            state++;
+            return state;
+          }
+          else return state;
+      break;
+
+      case 5:
+          return state;
+      break;
+
+      default:
+        return state;
+      break;
+  }
+}
+
+void getMessage(){
+  int state=0;
+  bool condition=false;
+  char aux;
+
+  while (!condition) {       /* loop for input */
+
+    res = read(fd,&aux, 1);   /* reads one character at a time */
+    printf("%X\n", aux);
+
+    state = stateMachine(aux, state);
+
+    if(state == 5) condition=true;
+  }
+
+  printf("Message: %x, %x, %x, %x, %x\n", message[0], message[1], message[2], message[3], message[4]);
+}
+
 int main(int argc, char** argv)
 {
-    int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
-	char aux;
-    bool condition=false;
 
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+    if ( (argc < 2) ||
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
   	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
 
-
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
-  
-    
+
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
 
@@ -63,13 +141,10 @@ int main(int argc, char** argv)
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+  /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
     leitura do(s) pr�ximo(s) caracter(es)
   */
-
-
 
     tcflush(fd, TCIOFLUSH);
 
@@ -80,105 +155,19 @@ int main(int argc, char** argv)
 
     printf("New termios structure set\n");
 
+    //get message from emissor
+    getMessage();
 
-    unsigned char CUA[5];
-    CUA[0]=FLAG;
-    CUA[1]=A;
-    CUA[2]=UA;
-    CUA[3]=CUA[1] ^ CUA[2];
+    //UA array
+    UA[0]=FLAG;
+    UA[1]=A;
+    UA[2]=C;
+    UA[3]=UA[1] ^ UA[2];
+    UA[4]= FLAG;
 
-
-    
-    int state=0;
-	while (!condition) {       /* loop for input */
-        res = read(fd,&aux, 1);   /* reads one character at a time */
-        printf("%X\n", aux);
-        
-        
-        
-        switch(state){
-            case 1:            
-                if(aux==FLAG){
-                    state++;
-                    continue;
-                }
-                else{
-                    continue;
-                }   
-            
-            
-            
-            break;
-
-            case 2:
-                if(aux==A){
-                    state++;
-                    continue;
-                }
-                else{
-                    continue;
-                }
-                
-
-            break;
-            
-            case 3:
-                if(aux==CSET){
-                    state++;
-                    continue;
-                }
-                else if(aux==FLAG){
-                    state--;
-                    continue;
-                }
-            
-            break;
-            
-            case 4:
-                if(aux==CUA[3]){
-                    state++;
-                    continue;
-                }
-
-                else{
-                    continue;
-                }
-                            
-            break;
-
-            case 5:
-                if(aux==FLAG){
-                    state++;
-                    continue;
-                }
-
-                else
-                {
-                    continue;
-                }
-                
-            break;
-
-
-            case 6:
-                condition=true;
-            break;
-            
-            default:
-            continue;
-            break;
-        }
-      
-      
-		
-    }
-
-
-  /* 
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o 
+  /*
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no gui�o
   */
-
-
 
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
